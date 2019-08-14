06673748dd62edd11a41a2aa6e0cd34e306a990b
David Toledo <dtoledoz0207@gmail.com>
2019-08-14 10:46:02 -0500

    edit-update-actions-linkTo-edit-Added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,11 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -18,6 +22,15 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
