ae0b7fb1241d0bb94339f932659d00acf4db96ca
David Toledo <dtoledoz0207@gmail.com>
2019-08-15 13:16:35 -0500

    http-authenticate-Articles-Comments-controller-Added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: 'david', password: 'secret', only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -11,7 +13,6 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article)
   end
-
 
   private
   def comment_params
